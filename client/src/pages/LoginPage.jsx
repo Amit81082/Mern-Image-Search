4ac9c039b2e7e88912bf4d3e8f405fddb13fae8a
Amit Maurya <amitkumarmaurya10122005@gmail.com>
2025-11-05 08:33:00 +0530

    google and github logo added in login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -14,9 +14,20 @@ function LoginPage() {
 
   return (
     <div className="login-container">
-      <h2>Welcome to Image Search App</h2>
-      <button onClick={handleGoogleLogin}>Login with Google</button>
-      <button onClick={handleGitHubLogin} className="github-btn">
+      <h2>Welcome to Image Search App 🖼️</h2>
+      <button onClick={handleGoogleLogin} className="login-btn google">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg"
+          alt="Google"
+        />
+        Login with Google
+      </button>
+
+      <button onClick={handleGitHubLogin} className="login-btn github">
+        <img
+          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+          alt="GitHub"
+        />
         Login with GitHub
       </button>
     </div>
